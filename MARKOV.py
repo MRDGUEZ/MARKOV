181c303cf76a0db8cfce548ee1d220c5fd4863b2
MRDGUEZ <124355860+MRDGUEZ@users.noreply.github.com>
2023-02-03 13:38:48 +0100

    Update MARKOV.py

--- MARKOV.py
+++ MARKOV.py
@@ -1,37 +1,37 @@
 import random
 
-def markov_generate_sentences(dict_words: dict[str : list[str]],dict_frases: dict[str: int] ,num_frases: int,generated_state:tuple):
-    frases_hechas={}
-    frase = list()
+def markov_generate_sentences(dict_words: dict[tuple : list[str]],dict_sentences: dict[str: int] ,number_of_sentences: int,generated_state:tuple)->None:
+    phrases_made={}
+    phrase = list()
     state = generated_state
     count = 1
-    attemps = 0
-    while count < num_frases+1 and attemps < 300 :
+    attempts = 0
+    while count < number_of_sentences+1 and attempts < 300 :
         word = random.choice(dict_words[state])
-        frase.append(word)
+        phrase.append(word)
         state = update_state(state,word)
 
         if not state in dict_words:
-            frase_complete=" ".join(frase)
-            if (not frase_complete in dict_frases) and (not frase_complete in frases_hechas):
-                print(f"{count}- "+ frase_complete,'\n')
-                frases_hechas[frase_complete]=count
-                frase.clear()
+            phrase_complete=" ".join(phrase)
+            if (not phrase_complete in dict_sentences) and (not phrase_complete in phrases_made):
+                print(f"{count}- "+ phrase_complete,'\n')
+                phrases_made[phrase_complete]=count
+                phrase.clear()
                 state=generated_state
                 count += 1
             else : 
-                frase.clear()
+                phrase.clear()
                 state = generated_state
-                attemps +=1
+                attempts +=1
 
-def introducir_clave_valor(anteriores: tuple, siguiente: str, dict_words: dict[str : list[str]]) -> dict[str : list[str]]:
-    if anteriores in dict_words:
-        dict_words[anteriores].append(siguiente)
+def enter_values_into_dict(state: tuple, word: str, dict_words: dict[tuple : list[str]]) -> dict[tuple: list[str]]:
+    if state in dict_words:
+        dict_words[state].append(word)
     else:
-        dict_words[anteriores] = [siguiente]
+        dict_words[state] = [word]
     return dict_words
 
-def eliminar_signos(line: str):
+def remove_signs(line: str)->str:
     separators = [",", ";", ":", ".", "-","?","!","/","\\","[","]","{","}",'\n']
     for sep in separators:
         line= line.replace(sep, "")
@@ -41,24 +41,24 @@ def state_generator(num_de_estados:int=2)-> tuple:
     state = ["START" for i in range(num_de_estados)]
     return tuple(state)
 
-def update_state (state:tuple[str],word:str):
+def update_state (state:tuple[str],word:str)->tuple:
     state=state[1:]
     state = state + (word,)
     return state
 
 def create_dict(file:list[str],generated_state:tuple)-> tuple[dict,dict]:
     state= generated_state
-    dict_words:dict[str : list[str]] = {}
-    dict_frases:dict[str : int] = {}
-    for j,line in enumerate (file) :
-        sentece:str = eliminar_signos(line)
-        dict_frases[sentece.strip()]=j   # generando diccionario de frases
+    dict_words:dict[tuple : list[str]] = {}
+    dict_sentences:dict[str : int] = {}
+    for index,line in enumerate (file) :
+        sentece:str = remove_signs(line)
+        dict_sentences[sentece.strip()]=index   # Generating word dictionary
         list_words:list[str] = sentece.rsplit()
         for word in list_words:
-            dict_words = introducir_clave_valor(state,word,dict_words) 
+            dict_words = enter_values_into_dict(state,word,dict_words) 
             state= update_state(state,word)
         state = generated_state
-    return dict_words,dict_frases      # DEVUELVE EL DICCIONARIO DE ESTADOS  CON LA LISTA DE PALABRAS Y ****UN DICCIONARIO CON LAS FRASES
+    return dict_words,dict_sentences      # Returns dictionary of states and dictionary of phrases as tuple
 
 def read_file(path: str) -> list[str]:
     try:
@@ -71,21 +71,21 @@ def read_file(path: str) -> list[str]:
         return lines
 
 if __name__ == "__main__":
-    #1 - Pedir numero de frases:
-    number_of_sentences:int = int(input("Cuantas frases quieres: "))
+    #1 - Ask for number of sentences:
+    number_of_sentences:int = int(input("Cuantas phrases quieres: "))
     print() 
-    #1.1 - Pedir estados:
+    #1.1 - Ask for number of states:
     number_of_states: int = int(input("Cuantos estados desea analizar: "))
     print()
 
-    #Generar estado:
+    #Generating state:
     generated_state:tuple = state_generator(number_of_states)
 
-    #3 - Leer archivo
+    #3 - Read the file
     file:list = read_file(r"/home/marcos/Downloads/prueba.txt")
 
-    #4 - Genero diccionario con las frases de mi archivo y otro diccionario con los {stados:[words]}
-    dict_words,dict_frases = create_dict(file,generated_state)
+    #4 - Generating diccionaries
+    dict_words,dict_sentences = create_dict(file,generated_state)
     
-    #5 - Genero cadenas
-    markov_generate_sentences(dict_words, dict_frases, number_of_sentences,generated_state)
+    #5 - Generating chains
+    markov_generate_sentences(dict_words, dict_sentences, number_of_sentences,generated_state)
